244e66ed78b80e6cf88cb7665b436c78696ae891
sila1404 <silamany1404@gmail.com>
2025-03-22 21:48:52 +0700

    fully design login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
 	Form,
@@ -10,15 +10,20 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
 
 function Login() {
 	const form = useForm();
 	return (
-		<Card className="flex flex-row gap-0 p-0 w-5/6 place-self-center">
-			<div className="flex-1 p-6">
-        <header className="text-primary font-bold text-xl">Login</header>
+		<Card className="flex flex-row gap-0 p-0 w-9/12 overflow-hidden shadow-[10px_18px_15px_rgba(0,0,0,0.1)]">
+			{/* Login Form */}
+			<div className="flex-1 px-6">
+				<header className="text-primary font-bold text-2xl my-12">Login</header>
+				{/* Email input */}
 				<Form {...form}>
-					<form action="" className="mx-3 my-6">
+					<form action="" className="mx-3 my-6 flex flex-col gap-6">
 						<FormField
 							control={form.control}
 							name="email"
@@ -28,44 +33,110 @@ function Login() {
 										<FormLabel>Email / Phone number</FormLabel>
 										<FormControl>
 											<Input
+												className="h-12"
 												placeholder="Enter your email or phone number"
 												{...field}
 											/>
 										</FormControl>
 									</FormItem>
-									<FormItem>
-										<FormLabel>Password</FormLabel>
-										<FormControl>
-											<Input
-												placeholder="Enter your password"
-												{...field}
-											/>
-										</FormControl>
-									</FormItem>
-                  <FormItem>
-                    <FormControl>
-                      <Checkbox />
-                    </FormControl>
-                    <FormLabel>
-                      Remember me
-                    </FormLabel>
-                  </FormItem>
 								</>
 							)}
 						/>
-            <Button type="submit" className="w-full">Login</Button>
+
+						{/* Password Input */}
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => {
+								const [showPassword, setShowPassword] = useState(false);
+
+								return (
+									<>
+										<FormItem>
+											<FormLabel>Password</FormLabel>
+											<FormControl>
+												<div className="relative">
+													<Input
+														type={showPassword ? "text" : "password"}
+														placeholder="Enter your password"
+														className="pr-10 h-12"
+														{...field}
+													/>
+
+													<button
+														type="button"
+														className="absolute grid place-items-center h-9 w-9 rounded-sm right-0 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+														onClick={() => setShowPassword(!showPassword)}
+													>
+														{showPassword ? (
+															<EyeIcon className="h-4 w-4" />
+														) : (
+															<EyeOffIcon className="h-4 w-4" />
+														)}
+													</button>
+												</div>
+											</FormControl>
+										</FormItem>
+									</>
+								);
+							}}
+						/>
+
+						{/* Checkbox and Forget password */}
+						<FormField
+							control={form.control}
+							name="remember"
+							render={({ field }) => (
+								<>
+									<FormItem className="flex flex-row justify-between">
+										<div className="flex flex-row">
+											<FormControl className="self-center mr-3">
+												<Checkbox
+													className="border-2 h-5 w-5 cursor-pointer"
+													checked={field.value}
+													onCheckedChange={field.onChange}
+												/>
+											</FormControl>
+											<FormLabel>Remember me</FormLabel>
+										</div>
+
+										<Link to={"#"} className="text-primary font-semibold">
+											Forget passsword
+										</Link>
+									</FormItem>
+								</>
+							)}
+						/>
+
+						{/* Submit Button */}
+						<Button type="submit" className="w-full my-6 py-6">
+							Sign In
+						</Button>
 					</form>
 				</Form>
-              <Button variant={"secondary"}>Create new account</Button>
+				<div className="flex justify-center w-full mb-12">
+					<Button variant={"secondary"} className="py-6 px-8">
+						<Link to={"/auth/signup"}>Create new account</Link>
+					</Button>
+				</div>
 			</div>
-			<div className="flex-1 bg-linear-to-b from-primary to-[#44339e]">
-				<CardHeader className="text-accent">
-					<h1>Mineral Tracking System</h1>
-					<p>
-						manage douments, request quotas, track shipments and view summary
+
+			{/* Description */}
+			<div className="flex-1 bg-linear-to-b from-primary to-[#44339e] flex flex-col justify-between p-6">
+				<CardHeader className="flex flex-col justify-center items-center text-white text-center mt-10">
+					<h1 className="text-3xl font-bold">Mineral Tracking System</h1>
+					<p className="text-sm">
+						Manage douments, request quotas, track shipments and view summary
 						report in one place
 					</p>
 				</CardHeader>
+				<CardFooter>
+					<img
+						src="/src/assets/images/truck.png"
+						alt="Trucks"
+						className="w-md m-auto"
+					/>
+				</CardFooter>
 			</div>
 		</Card>
 	);
